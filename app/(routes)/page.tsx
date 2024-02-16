3aea1006f45d6ff8291e4d88091e12e59ad14654
Grant_Cheng <cccguo97@gmail.com>
2024-02-16 16:49:09 +1000

    Add store home page layout

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,24 @@
-const HomePage = () => {
-  return <div>Hello World</div>
+import Container from "@/components/ui/Container"
+import Billboard from "@/components/Billboard"
+import ProductList from "@/components/ProductList"
+import getBillboard from "@/actions/getBillboard"
+import getProducts from "@/actions/getProducts"
+
+export const revalidate = 0
+
+const HomePage = async () => {
+  const billboard = await getBillboard("7d2146c8-8fd1-4c9c-b843-0cf330aa4d07")
+  const products = await getProducts({ isFeatured: true })
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
+      </div>
+    </Container>
+  )
 }
 
 export default HomePage
---
+++ components/ui/ProductCard.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import { Product } from "@/types"
+import Image from "next/image"
+import IconButton from "@/components/ui/IconButton"
+import Currency from "@/components/ui/Currency"
+import { Expand, ShoppingCart } from "lucide-react"
+
+interface ProductCardProps {
+  data: Product
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+  return (
+    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+      {/* Images And Actions */}
+      <div className="relative aspect-square rounded-xl bg-gray-100">
+        <Image
+          src={data?.images?.[0]?.url}
+          fill
+          alt="Image"
+          className="object-cover rounded-md aspect-square"
+        />
+        <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
+          <div className="flex gap-x-6 justify-center">
+            <IconButton
+              onClick={() => {}}
+              icon={<Expand size={20} className="text-gray-600" />}
+            />
+            <IconButton
+              onClick={() => {}}
+              icon={<ShoppingCart size={20} className="text-gray-600" />}
+            />
+          </div>
+        </div>
+      </div>
+      {/* Product Info */}
+      <div>
+        <p className="text-lg font-semibold">{data.name}</p>
+        <p className="text-sm text-gray-500">{data.category?.name}</p>
+      </div>
+      {/* Price */}
+      <div className="flex justify-between items-center">
+        <Currency value={data.price} />
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard
