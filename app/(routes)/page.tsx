8db43c210d959be68693be92af1044b037616185
Grant_Cheng <cccguo97@gmail.com>
2024-02-20 18:25:32 +1000

    Fix initial page load bug

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -7,7 +7,16 @@ import getProducts from "@/actions/getProducts"
 export const revalidate = 0
 
 const HomePage = async () => {
-  const billboard = await getBillboard("7d2146c8-8fd1-4c9c-b843-0cf330aa4d07")
+  const billboard = {
+    id: "caf1251d-23bc-445b-a7d7-8de043ee6f72",
+    storeId: "3d6cb075-fda3-4fd2-a247-1cb7d8c2558a",
+    label: "New Arrival",
+    imageUrl:
+      "https://res.cloudinary.com/djhhzsnda/image/upload/v1708415454/qu5jgakkj4gnjbmnztza.jpg",
+    createdAt: "2024-02-20T07:51:20.625Z",
+    updatedAt: "2024-02-20T07:51:20.625Z",
+  }
+
   const products = await getProducts({ isFeatured: true })
   return (
     <Container>
